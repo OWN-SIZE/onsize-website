83ee53bb2fa420fbe23f0bebb32e53842ab5a7ca
seobbang <seobbangway@naver.com>
2023-01-01 23:11:58 +0900

    feat/#4: 메뉴 클릭시 반응형 ui 및 페이지 이동 구현

--- components/common/MenuBar.tsx
+++ components/common/MenuBar.tsx
@@ -1,11 +1,28 @@
+import { MouseEvent } from 'react';
+import { useState } from 'react';
+import router from 'next/router';
 import styled from 'styled-components';
 import theme from 'styles/theme';
 
 export default function MenuBar() {
+  const [isClicked, setIsClicked] = useState('home');
+  const handleOnClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const newPage = e.currentTarget.id;
+    if (newPage === isClicked) {
+      return;
+    }
+    setIsClicked(newPage);
+    router.push(`/${newPage}`);
+  };
+
   return (
     <Styled.Root>
-      <Styled.Menu>나의 옷장</Styled.Menu>
-      <Styled.Menu>카테고리</Styled.Menu>
+      <Styled.Menu isClicked={isClicked === 'home' ? true : false} id="home" onClick={handleOnClick}>
+        나의 옷장
+      </Styled.Menu>
+      <Styled.Menu isClicked={isClicked === 'category' ? true : false} id="category" onClick={handleOnClick}>
+        카테고리
+      </Styled.Menu>
     </Styled.Root>
   );
 }
@@ -26,10 +43,12 @@ const Styled = {
     height: 6.1rem;
 
     background: none;
+
     border: none;
 
     ${theme.fonts.title2}
 
-    border-bottom :  ${({ isClicked }) => (isClicked ? '0.3rem solid black' : 0)};
+    border-bottom: 0;
+    border-bottom: ${({ isClicked }) => (isClicked ? '0.3rem solid black' : 0)};
   `,
 };
