f6224c39edd7fb7b4847af197bf4d15c7f1ce159
Eunhyung Choi <milkiss0414@naver.com>
2023-01-02 23:09:12 +0900

    feat/#3: localStorage에 값 저장

--- hooks/queries/user.ts
+++ hooks/queries/user.ts
@@ -5,6 +5,8 @@ export const useLoginMutation = (accessToken: string) => {
   return useMutation(() => axios.post('/auth/login', accessToken), {
     onSuccess(response) {
       console.log(response.data);
+      localStorage.setItem('isLogin', 'true');
+      localStorage.setItem('isRegister', 'false');
       // 초기 사이즈 설정 페이지로 이동하기
     },
   });
