b5ac17f7e4e441493abda455d9371630b9141d8b
borimong <kimhyunsu11@naver.com>
2023-01-02 20:35:09 +0900

    Git stash 를 모르기 전.. 메인 브랜치로 이동하기 위한 커밋

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,7 +1,35 @@
-import Header from 'components/common/Header';
+import Layout from 'components/common/Layout';
+import Image from 'next/image';
+import styled from 'styled-components';
+import theme from 'styles/theme';
+import profileDefault from 'assets/icon/profileDefault.png'
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <Layout noMenuBar>
+      <Styled.Root>
+        <Styled.PersonalInformation>
+          <Image src={profileDefault} alt="사용자 지정 프로필 이미지가 없는 경우의 디폴트 프로필 이미지" width={82} height={82} placeholder="blur"/>
+        </Styled.PersonalInformation>
+      </Styled.Root>
+    </Layout>
+  );
 }
 
 export default MyPage;
+
+const Styled = {
+  Root: styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100vw;
+    height: 100vh;
+    margin: 0 auto;
+  `,
+  PersonalInformation: styled.div`
+    display: flex;
+    width: 69.2rem;
+    height: 8.3rem;
+  `,
+}
