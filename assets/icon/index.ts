8a2ba9505879347f249c1b973636a6a53a0c02dc
seobbang <seobbangway@naver.com>
2023-01-04 13:26:06 +0900

    feat/#7: 옷 개수 아이콘 svg파일로 변경

--- assets/icon/index.ts
+++ assets/icon/index.ts
@@ -1,3 +1,3 @@
 // 명명규칙은 xxxxIcon으로 통일
 export { default as InfoIcon } from './info.png';
-export { default as ClosetIcon } from './closetIcon.png';
+export { default as ClosetIcon } from './closetIcon.svg';
