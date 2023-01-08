9509ae2728092ecc44b2d6cf22a9ae2e354f021f
borimong <kimhyunsu11@naver.com>
2023-01-08 15:30:18 +0900

    feat: modal portal root div 위치 조정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { useRef } from 'react';
 
 export default function Document() {
+
   return (
     <Html lang="ko">
       <Head>
