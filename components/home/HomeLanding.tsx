849e95f3da0cbcb584c9f60f07a84060aaf5fc67
seobbang <seobbangway@naver.com>
2023-01-09 00:35:40 +0900

    feat/#33: 상품 최신순 구현

--- components/home/HomeLanding.tsx
+++ components/home/HomeLanding.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { AllClosetOutput } from 'types/allCloset/client';
 
 import { useFetchAllCloset } from '@/hooks/queries/allCloset';
 
@@ -6,7 +7,18 @@ import HomeFirst from './HomeFirst';
 import HomeMain from './HomeMain';
 
 function HomeLanding() {
-  const data = useFetchAllCloset();
+  const orderSort = (item: AllClosetOutput[]) => {
+    return item.sort((a, b) => {
+      return Number(b.id) - Number(a.id);
+    });
+  };
+
+  let data = useFetchAllCloset();
+  if (data) {
+    const pinData: AllClosetOutput[] = orderSort(data.filter((data) => data.isPin === true));
+    const noPinData: AllClosetOutput[] = orderSort(data.filter((data) => data.isPin === false));
+    data = pinData.concat(noPinData);
+  }
   console.log(data);
 
   return <Styled.Root>{!data ? <HomeFirst /> : <HomeMain data={data} page="closet" />}</Styled.Root>;
