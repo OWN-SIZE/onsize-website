6fdae92e71b00d719dd07300a8341a883781083c
leeseooo <leeseoooo@naver.com>
2023-01-11 10:57:43 +0900

    feat/#37: 에러 페이지 생성

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
 import { GoogleLoginImg } from 'assets/img';
 //import { useLoginMutation } from 'hooks/queries/user';
@@ -22,6 +22,10 @@ function Login() {
     },
     flow: 'auth-code',
   });
+
+  useEffect(() => {
+    // 내가 아는 정보 : userId, token, isRegister(정보모두입력여부)
+  }, []);
 
   return (
     <Layout noHeader noMenuBar>
