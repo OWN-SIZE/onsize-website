1c3c7539e8d8a139b196cb33cbe95daad1a9dc25
seobbang <seobbangway@naver.com>
2023-01-13 08:39:44 +0900

    feat/#58: showToast 전달

--- components/home/Product.tsx
+++ components/home/Product.tsx
@@ -56,6 +56,7 @@ function Product(props: ProductProps) {
           page="closet"
           updateIsPin={updateIsPIn}
           setIsProductHovered={setIsProductHovered}
+          showToast={showToast}
         />
       ) : (
         categoryId && (
@@ -68,6 +69,7 @@ function Product(props: ProductProps) {
             updateIsPin={updateIsInPin}
             setIsProductHovered={setIsProductHovered}
             noAddCategory
+            showToast={showToast}
           />
         )
       )}
