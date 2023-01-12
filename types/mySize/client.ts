3b59c637ea6e6b39bee6db6f61791d52fb8166dd
Eunhyung Choi <milkiss0414@naver.com>
2023-01-13 06:14:09 +0900

    fix: sizeForm 타입에러

--- types/mySize/client.ts
+++ types/mySize/client.ts
@@ -1,4 +1,5 @@
 export interface TopSizeInput {
+  [key: string]: number | boolean;
   topLength: number;
   shoulder: number;
   chest: number;
@@ -6,6 +7,7 @@ export interface TopSizeInput {
 }
 
 export interface BottomSizeInput {
+  [key: string]: number | boolean;
   bottomLength: number;
   waist: number;
   thigh: number;
@@ -23,4 +25,3 @@ export interface MySizeOutput {
 export interface MySizeResponse {
   data: MySizeOutput;
 }
-
