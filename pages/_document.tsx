91d3eeddee64076794d00ec6583ae1f715118aa3
leeseooo <leeseoooo@naver.com>
2023-01-13 12:31:37 +0900

    feat/#64: favIcon 및 title 등록

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,8 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
-        <meta name="description" content="On Size" />
+        <meta name="description" content="쉽고 똑똑한 나만의 쇼핑 도우미, 온사이즈" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
@@ -16,6 +17,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <meta name="description" content="Own Size" />
       </Head>
       <body>
         <div id="modal-root"></div>
