67aeada82fd12b165d50a36c10b95573d792397a
Eunhyung Choi <milkiss0414@naver.com>
2023-01-31 18:11:27 +0900

    feat/#80: 리다이렉트 로딩속도 개선

--- hooks/common/useRedirect.ts
+++ hooks/common/useRedirect.ts
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Router, useRouter } from 'next/router';
 
 type LocalStorageType = string | null;
 
@@ -8,21 +9,34 @@ interface RedirectProps {
 
 function useRedirect(props: RedirectProps) {
   const { onRedirect } = props;
-  const [userId, setUserId] = useState<LocalStorageType>();
-  const [token, setToken] = useState<LocalStorageType>();
-  const [isRegister, setIsRegister] = useState<LocalStorageType>();
+  const [userId, setUserId] = useState<LocalStorageType>(null);
+  const [token, setToken] = useState<LocalStorageType>(null);
+  const [isRegister, setIsRegister] = useState<LocalStorageType>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const router = useRouter();
 
   const getLocalStorage = () => {
     setIsLoading(true);
     setUserId(localStorage.getItem('userId'));
     setToken(localStorage.getItem('token'));
     setIsRegister(localStorage.getItem('isRegister'));
+    onRedirect();
   };
 
   useEffect(() => {
-    onRedirect();
-  }, [userId, token, isRegister]);
+    const handleStart = (url: string) => url !== router.asPath && setIsLoading(true);
+    const handleComplete = (url: string) => url === router.asPath && setIsLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  });
 
   return { userId, token, isRegister, isLoading, setIsLoading, getLocalStorage };
 }
