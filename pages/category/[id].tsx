547ba71044abec9384cfe5f34a96698b6bd66143
seobbang <seobbangway@naver.com>
2023-02-06 01:07:19 +0900

    fix/#86: 수정불가 이슈로 인해 api 요청 방법으로 변경

--- pages/category/[id].tsx
+++ pages/category/[id].tsx
@@ -3,18 +3,25 @@ import { useSetRecoilState } from 'recoil';
 import { categoryIdState, categoryNameState } from 'states/categoryDetail';
 
 import CategoryDetailLanding from '@/components/category/detail/CategoryDetailLanding';
+import { useFetchAllCategory } from '@/hooks/queries/category';
 import Layout from 'components/common/Layout';
 
 function Detail() {
-  const {
-    query: { id, name },
-  } = useRouter();
-
   const setCategoryId = useSetRecoilState(categoryIdState);
   const setCategoryName = useSetRecoilState(categoryNameState);
 
+  const {
+    query: { id },
+  } = useRouter();
+
   setCategoryId(id as string);
-  setCategoryName(name as string);
+
+  const { category } = useFetchAllCategory();
+  if (category) {
+    category.map((item) => {
+      if (String(item.id) === id) setCategoryName(item.categoryName);
+    });
+  }
 
   return (
     <Layout noMenuBar>
