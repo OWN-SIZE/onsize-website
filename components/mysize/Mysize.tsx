6f80bdb5524a1ebb08f092c6a2bf968397052b32
borimong <kimhyunsu11@naver.com>
2023-02-10 03:43:15 +0900

    fix: 마이사이즈 상/하의 중 하나만 입력 시 발생하는 데이터 오전송 문제 해결

--- components/mysize/Mysize.tsx
+++ components/mysize/Mysize.tsx
@@ -153,6 +153,12 @@ export default function Mysize() {
         });
       } else if (isTopClicked === false && isWidthOfBottom === false && clickedMeasure === '둘레') {
         setData({ 총장: bottomLength, 밑위: rise, 허리: waist, 허벅지: thigh, 밑단: hem });
+      } 
+      else if (isTopClicked === false && isWidthOfBottom === null) {
+        setData({ 총장: bottomLength, 밑위: rise, 허리: waist, 허벅지: thigh, 밑단: hem });
+      }
+      else if (isTopClicked && isWidthOfTop === null) {
+        setData({ 총장: bottomLength, 밑위: rise, 허리: waist, 허벅지: thigh, 밑단: hem });
       }
     }
   }, [allMysize, isTopClicked, clickedMeasure]);
