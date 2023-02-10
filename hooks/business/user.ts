daf5f0e62ef8983279d7429a3740b277376ff8d0
leeseooo <leeseoooo@naver.com>
2023-02-10 23:16:30 +0900

    hotfix: setToken 추가

--- hooks/business/user.ts
+++ hooks/business/user.ts
@@ -1,17 +1,19 @@
-import Cookies from 'js-cookie';
+import { useRecoilState } from 'recoil';
+import { tokenState } from 'states/user';
 import { AuthInput } from 'types/user/client';
 
 import { useAuthMutation } from '../queries/user';
 
 export const useAuth = () => {
   const authMutate = useAuthMutation();
+  const [, setToken] = useRecoilState(tokenState);
 
   const authLogin = (body: AuthInput, onSuccessLogin: () => void) => {
     authMutate.mutate(body, {
       onSuccess({ userId, token }) {
         localStorage.setItem('userId', `${userId}`);
         localStorage.setItem('token', `${token}`);
-        Cookies.set('token', token);
+        setToken(token);
 
         // 초기 사이즈 설정 페이지로 이동하기
         onSuccessLogin();
