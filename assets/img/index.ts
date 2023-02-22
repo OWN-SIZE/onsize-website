c522a7132f8f1b8ea3efcc995991734181e6a408
seobbang <seobbangway@naver.com>
2023-02-22 23:50:27 +0900

    feat/#125: 이미지 webp 변환

--- assets/img/index.ts
+++ assets/img/index.ts
@@ -1,15 +1,15 @@
 // 명명규칙은 xxxxImg로 통일
-export { default as ChromeWebstoreImg } from './chrome-webstore.png';
-export { default as GoogleLoginImg } from './google-login.png';
-export { default as TopBottomImg } from './top-bottom.png';
-export { default as TopImg } from './top.png';
-export { default as BottomImg } from './bottom.png';
-export { default as Hanger } from './hanger.png';
+export { default as ChromeWebstoreImg } from './chrome-webstore.webp';
+export { default as GoogleLoginImg } from './google-login.webp';
+export { default as TopBottomImg } from './top-bottom.webp';
+export { default as TopImg } from './top.webp';
+export { default as BottomImg } from './bottom.webp';
+export { default as Hanger } from './hanger.webp';
 export { default as OwnSizeLogoImg } from './ownsizeLogo.webp';
-export { default as LoginMouseImg } from './login-mouse.png';
-export { default as BigLogoImg } from './logo-big.png';
-export { default as MouseImg } from './mouse.png';
-export { default as MouseHoverImg } from './mouse_hover.png';
+export { default as LoginMouseImg } from './login-mouse.webp';
+export { default as BigLogoImg } from './logo-big.webp';
+export { default as MouseImg } from './mouse.webp';
+export { default as MouseHoverImg } from './mouse_hover.webp';
 export { default as desktopBackgroundImg } from './web_main.webp';
 export { default as ArrowImg } from './arrow.webp';
 export { default as mobileBackgroundImg } from './mobile_main.webp';
