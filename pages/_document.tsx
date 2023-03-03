b6b546e9b8ea81d30807585167e0f475f89ee10b
borimong <kimhyunsu11@naver.com>
2023-03-03 16:17:10 +0900

    feat: meta 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,8 +33,10 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
+          <title>온사이즈 OWNSIZE | 옷 사이즈 고민 없는 온라인 쇼핑의 시작</title>
           <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
-          <meta name="description" content="쉽고 똑똑한 나만의 쇼핑 도우미, 온사이즈" />
+          <meta name="description" content="옷마다 브랜드마다 모두 다른 사이즈 체계, 온사이즈에서는 나에게 꼭 맞는 사이즈를 1초 만에 알려드려요. 사이즈 추천 받고 위시리스트 저장하고 관리까지 해보세요. 6개의 사이트(무신사, 29cm, 오케이몰, SSENSE, Mr.Porter, w컨셉)에서 1초 만에 나의 옷 사이즈를 알려드려요. 미국 옷 사이즈, 유럽 옷 사이즈, 빅사이즈 등 사이즈를 알기 까다로운 의류에서 이용해 보세요. 옷 사이즈는 재는법, 옷 사이즈표, 위시리스트" />
+          <meta name="keywords" content="위시리스트, 옷사이즈, 온라인쇼핑, 미국 옷 사이즈, 유럽 옷 사이즈, 빅사이즈, 옷 사이즈 재는법, 옷 사이즈표" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -45,7 +47,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-          <meta name="description" content="Own Size" />
         </Head>
         <body style={{ overflow: 'hidden' }}>
           <noscript>
